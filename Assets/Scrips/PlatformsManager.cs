d46bf293ed228d0fdc37b4a259c88c94e631eb3a
Eduardo340-u <eduardo.chavez@uteca.edu.mx>
2026-02-04 20:36:51 -0600

    04febrero

--- Assets/Scrips/PlatformsManager.cs
+++ Assets/Scrips/PlatformsManager.cs
@@ -5,7 +5,7 @@ public class PlatformsManager : MonoBehaviour
     [SerializeField]
     private Transform platformsPivot;
     [SerializeField]
-    private GameObject[] platformPrefabs;
+    private InstantiatePoolObjects[] platformPrefabs;
     [SerializeField]
     private int initialPlataforms = 5;
     [SerializeField]
@@ -21,13 +21,15 @@ public class PlatformsManager : MonoBehaviour
     {
         for (int i=0; i < number; i++)
         {
-            GameObject platformPrefab = platformPrefabs[Random.Range(0, platformPrefabs.Length)];
+            InstantiatePoolObjects instantiatePool = platformPrefabs[Random.Range(0, platformPrefabs.Length)];
             Vector3 spawnPosition = Vector3.zero;
             if (lastPlataform !=null)
             {
                 spawnPosition = lastPlataform.transform.localPosition + lastPlataform.GetComponent<Collider>().bounds.size.z * Vector3.forward * 0.5f;
             }
-            GameObject newPlatform = Instantiate(platformPrefab, Vector3.zero, Quaternion.identity, transform);
+            instantiatePool.InstantiateObject(spawnPosition);
+            GameObject newPlatform = instantiatePool.GetCurrentObject();
+            newPlatform.transform.SetParent(transform);
             newPlatform.transform.localPosition = spawnPosition + newPlatform.GetComponent<Collider>().bounds.size.z * Vector3.forward * 0.5f;
             lastPlataform = newPlatform;
         }
@@ -36,7 +38,11 @@ public class PlatformsManager : MonoBehaviour
     {
         if (isRunning)
         {
-            platformsPivot.Translate(Vector3.back * speed * Time.deltaTime);
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
+    }
+    public void StopPlatforms()
+    {
+        isRunning = false;
     }
 }
