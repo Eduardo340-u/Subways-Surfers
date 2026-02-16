75f966ee6c2c1add9af392db0367647683a91e82
Eduardo340-u <eduardo.chavez@uteca.edu.mx>
2026-02-15 18:38:13 -0600

    15Feb

---
+++ Subways-Surfers/Assets/Scrips/Character.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+using DG. Tweening;
+using System.Collections;
+using UnityEngine.Events;
+
+public class Character : MonoBehaviour
+{
+    private Rigidbody characterRigidbody;
+    [SerializeField]
+    private Data characterData;
+    [SerializeField]
+    private Animator characterAnimator;
+    [SerializeField]
+    private float jumpForce = 5f;
+    [SerializeField]
+    private float distanceToMove = 2f;
+    [SerializeField]
+    private float moveDuration = 0.2f;
+    [SerializeField]
+    private Transform ChararacterStartPivot;
+    [SerializeField]
+    private UnityEvent onJump;
+    [SerializeField]
+    private UnityEvent onMoveToSide;
+    [SerializeField]
+    private UnityEvent onRoll;
+    [SerializeField]
+    private UnityEvent onLose;
+    private bool isGrounded= true;
+    private bool isMoving = false;
+    private bool isRolling = false;
+    private bool isActive = false;
+    private void Awake()
+    {
+        characterRigidbody = GetComponent<Rigidbody>();
+    }
+    public void StartGame()
+    {
+        isRolling = false;
+        isMoving = false;
+        isActive = true;
+        characterAnimator.Play(characterData.runAnimationName, 0, 0f);
+        transform.position= ChararacterStartPivot.position;
+    }
+    private void Start()
+    {
+        isActive = true;
+        characterAnimator.Play(characterData.runAnimationName, 0, 0f);
+        characterRigidbody = GetComponent<Rigidbody>();
+    }
+    public void Lose()
+    {
+        isActive = false;
+        StopAllCoroutines();
+        characterAnimator.Play(characterData.loseAnimationName, 0, 0f);
+    }
+    public void Jump()
+    {
+        if (!isActive) return;
+        if (isGrounded)
+        {
+            onJump?.Invoke();
+            characterAnimator.Play(characterData.jumpAnimationName, 0, 0f);
+            characterRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+    public void MoveDown()
+    {
+        if (!isActive || isRolling) return;
+        if (!isGrounded)
+        {
+            characterRigidbody.AddForce(Vector3.down * jumpForce * 2, ForceMode.Impulse);
+        }
+        characterAnimator.Play(characterData.rollAnimationName, 0, 0f);
+        onRoll?.Invoke();
+        isRolling = true;
+        StartCoroutine(ResetRoll());
+    }
+    public void MoveLeft()
+    {
+        if (transform.position.x <= -distanceToMove) return;
+        Move(Vector3.left);
+    }
+    public void MoverRight()
+    {
+        if (transform.position.x >= distanceToMove) return;
+        Move(Vector3.right);
+    }
+    private void Move(Vector3 direction)
+    {
+        if (isMoving || !isActive) return;
+        onMoveToSide?.Invoke();
+        characterAnimator.Play(characterData.moveAnimationName, 0, 0f);
+        isMoving= true;
+        Vector3 targetPosition = transform.position + direction * distanceToMove;
+
+        transform.DOMove(targetPosition, moveDuration).SetEase(Ease.OutQuad).OnComplete(()=>
+        {
+           isMoving = false; 
+        });
+    }
+    private IEnumerator ResetRoll()
+    {
+        yield return new WaitForSeconds(characterAnimator.GetCurrentAnimatorStateInfo(0).length);
+        isRolling = false;
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (isActive && collision.gameObject.CompareTag("Ground"))
+        {
+            if (!isRolling)
+            {
+                characterAnimator.Play(characterData.runAnimationName, 0, 0f);
+            }
+            isGrounded = true;
+        }
+    }
+}
---
+++ Subways-Surfers/Assets/Scrips/PlatformsManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlatformsManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform platformsPivot;
+    [SerializeField]
+    private InstantiatePoolObjects[] platformPrefabs;
+    [SerializeField]
+    private int initialPlataforms = 5;
+    [SerializeField]
+    private float speed = 5f;
+    private bool isRunning = true;
+    private GameObject lastPlataform;
+    public void StartGame()
+    {
+        lastPlataform = null;
+        InitializePlatforms();
+        InstantiatePlatform(initialPlataforms);
+        transform.position = platformsPivot.position;
+        isRunning = true;
+    }
+    private void InitializePlatforms()
+    {
+        foreach (var platform in platformPrefabs)
+        {
+            platform.DeactivateAllObjects();
+        }
+    }
+    public void InstantiatePlatform(int number)
+    {
+        for (int i=0; i < number; i++)
+        {
+            InstantiatePoolObjects instantiatePool = platformPrefabs[Random.Range(0, platformPrefabs.Length)];
+            Vector3 spawnPosition = Vector3.zero;
+            if (lastPlataform !=null)
+            {
+                spawnPosition = lastPlataform.transform.localPosition + lastPlataform.GetComponent<Collider>().bounds.size.z * Vector3.forward * 0.5f;
+            }
+            instantiatePool.InstantiateObject(spawnPosition);
+            GameObject newPlatform = instantiatePool.GetCurrentObject();
+            newPlatform.transform.SetParent(transform);
+            newPlatform.transform.localPosition = spawnPosition + newPlatform.GetComponent<Collider>().bounds.size.z * Vector3.forward * 0.5f;
+            lastPlataform = newPlatform;
+        }
+    }
+    private void Update() 
+    {
+        if (isRunning)
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+    }
+    public void StopPlatforms()
+    {
+        isRunning = false;
+    }
+}
