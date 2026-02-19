fa67dd495694225b65aca9ebd628fe330a368aee
Eduardo340-u <eduardo.chavez@uteca.edu.mx>
2026-02-18 20:35:16 -0600

    feb18

--- Assets/Scrips/PlatformsManager.cs
+++ Assets/Scrips/PlatformsManager.cs
@@ -7,14 +7,18 @@ public class PlatformsManager : MonoBehaviour
     [SerializeField]
     private InstantiatePoolObjects[] platformPrefabs;
     [SerializeField]
+    private InstantiatePoolObjects[] securePlatformPrefabs;
+    [SerializeField]
     private int initialPlataforms = 5;
     [SerializeField]
     private float speed = 5f;
     private bool isRunning = true;
     private GameObject lastPlataform;
+    private int platformIstantiated = 0;
     public void StartGame()
     {
         lastPlataform = null;
+        platformIstantiated = 0;
         InitializePlatforms();
         InstantiatePlatform(initialPlataforms);
         transform.position = platformsPivot.position;
@@ -26,12 +30,24 @@ public class PlatformsManager : MonoBehaviour
         {
             platform.DeactivateAllObjects();
         }
+        foreach (var securePlatform in securePlatformPrefabs)
+        {
+            securePlatform.DeactivateAllObjects();
+        }
     }
     public void InstantiatePlatform(int number)
     {
         for (int i=0; i < number; i++)
         {
-            InstantiatePoolObjects instantiatePool = platformPrefabs[Random.Range(0, platformPrefabs.Length)];
+            InstantiatePoolObjects instantiatePool;
+            if (platformIstantiated < 2)
+            {
+                instantiatePool = securePlatformPrefabs[Random.Range(0, securePlatformPrefabs.Length)];
+            } else
+            {
+                instantiatePool = platformPrefabs[Random.Range(0, platformPrefabs.Length)];
+            }
+            platformIstantiated++;
             Vector3 spawnPosition = Vector3.zero;
             if (lastPlataform !=null)
             {
